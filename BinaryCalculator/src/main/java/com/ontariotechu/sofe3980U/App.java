f6bee90d725b195498d14cccc60f4f3d6241e609
Massimo Albanese <33076159+madlitch@users.noreply.github.com>
2023-02-13 20:54:25 -0500

    - completed App.java - completed Binary.java - completed BinaryTest.java

--- BinaryCalculator/src/main/java/com/ontariotechu/sofe3980U/App.java
+++ BinaryCalculator/src/main/java/com/ontariotechu/sofe3980U/App.java
@@ -1,54 +1,40 @@
 package com.ontariotechu.sofe3980U;
-import org.joda.time.LocalTime;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-      boolean cont = true;
-      long millis = System.currentTimeMillis();
-      java.util.Date date = new java.util.Date(millis);
-      System.out.println("The current local time is " +date);
-      while (cont=true) {
-      Scanner scan=new Scanner(System.in);
-      System.out.println("Select an operation: 1.Addition, 2.Or, 3.And, 4.multiplication, 5.exit ");
-      String reply=scan.next();
-      reply = reply.toLowerCase();
-      if (reply.equals("5") || reply.equals("exit")){
-         cont=false;
-         break;
-      }
-      System.out.println("Enter the first number:");
-      Binary binary1=new Binary(scan.next());
-      System.out.println("Enter the second number");
-      Binary binary2=new Binary(scan.next());
-      if (reply.equals("1") || reply.equals("addition")){
-         Binary sum= Binary.add(binary1,binary2);
-            System.out.println( "Their summation is "+sum.getValue());
-      }
-      if (reply.equals("2") || reply.equals("or")){
-	    Binary or= Binary.or(binary1,binary2);
-            System.out.println( "Their or is "+or.getValue());
 
-      }
-      if (reply.equals("3") || reply.equals("and")){
-      Binary and=Binary.and(binary1,binary2);
-       System.out.println("Their and is " +and.getValue());
-       } 
-       
-      if (reply.equals("4") || reply.equals("muliplication")){
-	Binary multiply = Binary.multiply(binary1,binary2);
-	System.out.println("Their multiplcation is: " + multiply.getValue());
-      }
-      
+public class App {
+    public static void main(String[] args) throws Exception {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
+        System.out.println("The current local time is: " + dateTimeFormatter.format(LocalDateTime.now()));
 
-       }
-       System.out.println("Goodbye");
+        Scanner scan = new Scanner(System.in);
+        List<String> options = Arrays.asList("1", "2", "3", "4");
+
+        while (true) {
+            System.out.println("Select an Operation:");
+            System.out.println("1. Add \n2. Or \n3. And \n4. Multiply \n5. Exit ");
+            String reply = scan.next().toLowerCase();
+
+            if (reply.equals("5"))
+                break;
+            else if (!options.contains(reply))
+                continue;
+
+            System.out.println("Enter the first number:");
+            Binary binary1 = new Binary(scan.next());
+            System.out.println("Enter the second number:");
+            Binary binary2 = new Binary(scan.next());
+
+            Binary result = switch (reply) {
+                case "1" -> Binary.add(binary1, binary2);
+                case "2" -> Binary.or(binary1, binary2);
+                case "3" -> Binary.and(binary1, binary2);
+                case "4" -> Binary.multiply(binary1, binary2);
+                default -> throw new Exception("Invalid Input");
+            };
+            System.out.println("The result is: " + result.getValue());
+        }
     }
 }
